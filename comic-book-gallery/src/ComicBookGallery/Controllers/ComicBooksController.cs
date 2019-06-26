2d0d077c022ff0e7c6b29b7a6d215bccbd8b73c5
Flo <Florianb@payspace.com>
2019-06-26 08:01:56 +0200

    Added Series List view

--- comic-book-gallery/src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,13 +1,13 @@
-﻿using ComicBookGallery.Data;
-using ComicBookGallery.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace ComicBookGallery.Controllers
+﻿namespace ComicBookGallery.Controllers
 {
+    using ComicBookGallery.Data;
+    using ComicBookGallery.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
     public class ComicBooksController : Controller
     {
         private ComicBookRepository _comicBookRepository = null;
---
+++ comic-book-gallery/src/ComicBookGallery/Controllers/SeriesController.cs
@@ -0,0 +1,27 @@
+﻿namespace ComicBookGallery.Controllers
+{
+    using ComicBookGallery.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class SeriesController : Controller
+    {
+        private SeriesRepository _seriesRepository = null;
+
+        //constructor
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GetSeries();
+
+            return View(series);
+        }
+    }
+}
